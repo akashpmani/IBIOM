9189318935acaa6e26d835e907a0a27cf0990cfb
akashpmani <akashpmani@gmail.com>
2023-10-03 18:33:18 +0530

    static/media

--- ibiom_backend/ibiom_backend/settings.py
+++ ibiom_backend/ibiom_backend/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from datetime import timedelta
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-ud64yu(c80uf1#an1dj@85)ktvrmg)bp@q!v7^$@4yfuhmpb-x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['52.66.91.209']
 
 AUTH_USER_MODEL = 'accounts.UserAccounts'
 
@@ -142,6 +143,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+MEDIA_URL ='/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
